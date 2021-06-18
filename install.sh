b24bc8891551a5bd7bed4bdd0ab99a56df0cf8ad
Thomas CASTELLY <thomas@tcy.io>
2021-06-18 16:18:47 +0200

    add env to specify root directory of project

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # script to download minimum files in current project
 
 mkdir config
-wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/config/nginx/default.conf -P ./config
+wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/config/nginx/default.conf -P ./config/nginx
 wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/.gitignore
 wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/docker-compose.yml
 wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/start.sh
