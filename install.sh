09614937ea0c8a119ee4630f95c4a5e065c44e02
Thomas CASTELLY <thomas@tcy.io>
2021-06-18 16:43:18 +0200

    replace wget by curl, preserve .gitignore if exist

--- install.sh
+++ install.sh
@@ -2,16 +2,25 @@
 
 # script to download minimum files in current project
 
-mkdir config
-wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/config/nginx/default.conf -P ./config/nginx
-wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/.gitignore
-wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/docker-compose.yml
-wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/start.sh
-wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/stop.sh
+mkdir -p config/nginx
 
-chmod +x ./start.sh
-chmod +x ./stop.sh
+curl https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/config/nginx/default.conf --output ./config/nginx/default.conf
+
+# download and concat .gitignore
+gitignore=$(curl https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/.gitignore)
+
+echo $gitignore >> .gitignore
+
+curl https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/docker-compose.yml --output ./docker-compose.yml
+
+curl https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/start.sh --output ./start.sh
+
+curl https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/stop.sh --output ./stop.sh
 
 if [ ! -f ./index.php ]; then
-    wget https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/src/index.php
+    curl https://raw.githubusercontent.com/shenron/php-mariadb-startup/master/src/index.php --output ./index.php
 fi
+
+chmod +x ./start.sh
+
+chmod +x ./stop.sh
